3000383a6dfaa2e40cb309c28977a7845c4e2821
Mousab <m.salah@unifiedideas.com>
2021-11-03 15:58:57 +0200

    Getting to Green

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Concert;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
