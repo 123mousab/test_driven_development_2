590a1a20836b068990281cfc952b4e9ae5154d76
Mousab <m.salah@unifiedideas.com>
2021-11-09 16:08:16 +0200

    Cancelling Reservations

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Models\Concert;
 use App\Models\Reservation;
+use App\Models\Ticket;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -30,5 +31,26 @@ class ReservationTest extends TestCase
         $reservation = new Reservation($tickets);
 
         $this->assertEquals(6000, $reservation->totalCost());
+    }
+
+    /**
+     * @test
+     */
+    public function reserved_tickets_are_released_when_a_reservation_is_cancelled()
+    {
+        $ticket1 = \Mockery::mock(Ticket::class);
+        $ticket1->shouldReceive('release')->once();
+
+        $ticket2 = \Mockery::mock(Ticket::class);
+        $ticket2->shouldReceive('release')->once();
+
+        $ticket3 = \Mockery::mock(Ticket::class);
+        $ticket3->shouldReceive('release')->once();
+
+        $tickets = collect([$ticket1, $ticket2, $ticket3]);
+
+        $reservation = new Reservation($tickets);
+
+        $reservation->cancel();
     }
 }
