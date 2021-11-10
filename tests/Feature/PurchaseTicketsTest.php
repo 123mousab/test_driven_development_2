abfee6e153465838adba2bb5a8a4093f07458585
Mousab <m.salah@unifiedideas.com>
2021-11-10 10:35:25 +0200

    Cancelling reservations

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -207,7 +207,9 @@ class PurchaseTicketsTest extends TestCase
      */
     function an_order_is_not_created_if_payment_fails()
     {
-        $concert = Concert::factory()->published()->create(['ticket_price' => 3250])->addTickets(3);
+        $concert = Concert::factory()->published()
+            ->create(['ticket_price' => 3250])
+            ->addTickets(3);
 
         $response = $this->orderTickets($concert, [
             'email' => 'john@example.com',
