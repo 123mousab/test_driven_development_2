8cbd3f88a544be8cc50b4c13cf8db2d6398c3ff2
Mousab <m.salah@unifiedideas.com>
2021-11-10 11:31:13 +0200

    Deleting Stale Tests

--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -29,22 +29,6 @@ class OrderTest extends TestCase
         $this->assertEquals(2, $concert->ticketsRemaining());
     }
 
-    /**
-     * @test
-     */
-    public function tickets_are_released_when_an_order_is_cancelled()
-    {
-        $concert = Concert::factory()->create()->addTickets(10);
-        $order = $concert->orderTickets('jane.example.com', 5);
-
-        $this->assertEquals(5, $concert->ticketsRemaining());
-
-        $order->cancel();
-
-        $this->assertEquals(10, $concert->ticketsRemaining());
-//        $this->assertNull(Order::find($order->id));
-        $this->assertFalse($concert->hasOrderFor('jane.example.com'));
-    }
 
     /**
      * @test
