27b3deb834b9443bb50f7e086ff180a99382522c
Mousab <m.salah@unifiedideas.com>
2021-11-24 15:35:48 +0200

    Integrating Hashids

---
+++ app/Billing/HashidsTicketCodeGenerator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Billing;
+
+use App\OrderConfirmationNumberGenerator;
+use App\TicketCodeGenerator;
+
+class HashidsTicketCodeGenerator implements TicketCodeGenerator
+{
+    private $hashids;
+
+    public function __construct($salt)
+    {
+        $this->hashids = new \Hashids\Hashids($salt, 6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    }
+
+    public function generateFor($ticket)
+    {
+        return $this->hashids->encode($ticket->id);
+    }
+}
