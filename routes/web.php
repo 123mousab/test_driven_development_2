6c62d72d0ab191cdbd5819c566bd70cd70cae7d2
Mousab <m.salah@unifiedideas.com>
2021-11-29 11:15:03 +0200

    Adding a Valid Concert

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('mockups/order', function (){
     return view('orders.show');
 });
 
-Route::get('/concerts/{id}', [\App\Http\Controllers\ConcertsController::class, 'show']);
+Route::get('/concerts/{id}', [\App\Http\Controllers\ConcertsController::class, 'show'])->name('concerts.show');
 Route::post('/concerts/{id}/orders', [\App\Http\Controllers\ConcertsOrdersController::class, 'store']);
 Route::get('/orders/{confirmationNumber}', [\App\Http\Controllers\OrderController::class, 'show']);
 
@@ -21,6 +21,7 @@ Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'login
 Route::get('/login', [\App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('auth.show-login');
 Route::post('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('auth.logout');
 
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/backstage/concerts/new', [\App\Http\Controllers\Backstage\ConcertController::class, 'create']);
+Route::prefix('backstage')->middleware(['auth'])->group(function() {
+    Route::get('concerts/new', [\App\Http\Controllers\Backstage\ConcertController::class, 'create']);
+    Route::post('concerts', [\App\Http\Controllers\Backstage\ConcertController::class, 'store']);
 });
