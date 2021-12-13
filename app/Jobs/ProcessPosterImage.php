8f7f9fc99a032b2686cbf1ef0e88151ed0b91e86
Mousab <m.salah@unifiedideas.com>
2021-12-13 13:14:31 +0200

    finish

---
+++ app/Jobs/ProcessPosterImage.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessPosterImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $concert;
+
+    public function __construct($concert)
+    {
+        $this->concert = $concert;
+    }
+
+    public function handle()
+    {
+        //
+    }
+}
